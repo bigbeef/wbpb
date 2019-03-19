25057fbda72ee111ee18b1d5620ecd456d12cee6
winfed <245655812@qq.com>
2019-03-19 19:23:39 +0800

    vue前端初始化

---
+++ wbpb-app/src/views/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="账号">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即上传</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      form: {}
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
+</script>
+
+<style type="less" scoped>
+.container{
+  width: 800px;
+  margin-left: 200px;
+}
+</style>
